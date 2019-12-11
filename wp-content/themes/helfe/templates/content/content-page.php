1375886779fa1b1f9dae62c7d6153598370d2cf6
Valentine Lav <valentine.lav@ipsos.com>
2019-12-11 17:58:06 +0100

    :ambulance: Fixing content

--- wp-content/themes/helfe/templates/content/content-page.php
+++ wp-content/themes/helfe/templates/content/content-page.php
@@ -12,12 +12,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! helfe_can_show_post_thumbnail() ) : ?>
-	<header class="entry-header">
-		<?php get_template_part( 'templates/header/entry', 'header' ); ?>
-	</header>
-	<?php endif; ?>
-
 	<div class="entry-content">
 		<?php
 		the_content();
