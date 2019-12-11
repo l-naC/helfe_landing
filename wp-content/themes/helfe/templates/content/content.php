4eb8da47979f8e16a5201ae22e825708e0ebaa64
Valentine Lav <valentine.lav@ipsos.com>
2019-12-11 17:49:10 +0100

    :art: -

--- wp-content/themes/helfe/templates/content/content.php
+++ wp-content/themes/helfe/templates/content/content.php
@@ -27,7 +27,20 @@
 
 	<div class="entry-content">
 		<?php
-		the_content();
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Post title. Only visible to screen readers. */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'helfe' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
 
 		wp_link_pages(
 			array(
