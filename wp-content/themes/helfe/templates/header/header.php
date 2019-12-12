a4e1e52cc6d2a8c965071bead6138ab3cf8d6e10
Valentine <valentine.lav@ipsos.com>
2019-12-12 17:18:35 +0100

    :ambulance: Fixing menu view and adapt it to bootstrap

--- wp-content/themes/helfe/templates/header/header.php
+++ wp-content/themes/helfe/templates/header/header.php
@@ -7,52 +7,62 @@
  * @since 1.0.0
  */
 ?>
-<div class="container-fluid">
-    <?php if ( has_custom_logo() ) : ?>
-		<div class="site-logo"><?php the_custom_logo(); ?></div>
+<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-light bg-light" aria-label="<?php esc_attr_e( 'Top Menu', 'helfe' ); ?>">
+<?php if ( has_custom_logo() ) : ?>
+	<div class="site-logo"><?php the_custom_logo(); ?></div>
+<?php endif; ?>
+<?php $blog_info = get_bloginfo( 'name' ); ?>
+<?php if ( ! empty( $blog_info ) ) : ?>
+	<?php if ( is_front_page() && is_home() ) : ?>
+		<a class="site-title navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+	<?php else : ?>
+		<a class="navbar-brand site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 	<?php endif; ?>
-	<?php $blog_info = get_bloginfo( 'name' ); ?>
-	<?php if ( ! empty( $blog_info ) ) : ?>
-		<?php if ( is_front_page() && is_home() ) : ?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		<?php else : ?>
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-		<?php endif; ?>
-	<?php endif; ?>
+<?php endif; ?>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+</button>
+<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
 	<?php
-	$description = get_bloginfo( 'description', 'display' );
-	if ( $description || is_customize_preview() ) :
+	$args = [
+		'theme_location'	=> 'menu-1',
+		'container'			=> 'div',
+		'container_class'	=> 'collapse navbar-collapse justify-content-end',
+		'container_id'		=> 'navbarSupportedContent',
+		'items_wrap'		=> '<ul id="%1$s" class="navbar-nav mr-auto %2$s">%3$s</ul>',
+		'add_li_class'		=> 'nav-item'
+	];
+	function add_additional_class_on_li($classes, $item, $args) {
+		if(isset($args->add_li_class)) {
+			$classes[] = $args->add_li_class;
+		}
+		return $classes;
+	}
+	add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+	function add_additional_class_on_a( $atts, $item, $args ) {
+		$class = 'nav-link'; // or something based on $item
+		$atts['class'] = $class;
+		return $atts;
+	}
+	add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
+
+	wp_nav_menu($args);
+	?>
+<?php endif; ?>
+</nav><!-- #site-navigation -->
+<?php if ( has_nav_menu( 'social' ) ) : ?>
+	<nav class="social-navigation nav justify-content-end" aria-label="<?php esc_attr_e( 'Social Links Menu', 'helfe' ); ?>">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'social',
+				'menu_class'     => 'social-links-menu',
+				'link_before'    => '<span class="screen-reader-text">',
+				'link_after'     => '</span>',
+				'depth'          => 1,
+			)
+		);
 		?>
-			<p class="site-description">
-				<?php echo $description; ?>
-			</p>
-	<?php endif; ?>
-    <?php if ( has_nav_menu( 'menu-1' ) ) : ?>
-		<nav id="site-navigation nav justify-content-end" class="main-navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'helfe' ); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_class'     => 'main-menu',
-					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	<?php endif; ?>
-	<?php if ( has_nav_menu( 'social' ) ) : ?>
-		<nav class="social-navigation nav justify-content-end" aria-label="<?php esc_attr_e( 'Social Links Menu', 'helfe' ); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'social',
-					'menu_class'     => 'social-links-menu',
-					'link_before'    => '<span class="screen-reader-text">',
-					'link_after'     => '</span>',
-					'depth'          => 1,
-				)
-			);
-			?>
-		</nav><!-- .social-navigation -->
-	<?php endif; ?>
-</div>
+	</nav><!-- .social-navigation -->
+<?php endif; ?>
