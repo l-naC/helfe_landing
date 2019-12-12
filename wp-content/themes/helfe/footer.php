47d45a63884aafd27bd501f3f1078c8fca9a245c
Valentine <valentine.lav@ipsos.com>
2019-12-12 12:35:07 +0100

    :lipstick: Updating Footer and Header views

--- wp-content/themes/helfe/footer.php
+++ wp-content/themes/helfe/footer.php
@@ -12,33 +12,37 @@
 ?>
 
 </div><!-- #content -->
-FOOTER
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer page-footer">
 		<?php get_template_part( 'templates/footer/footer', 'widgets' ); ?>
-		<div class="site-info">
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
+		<div class="site-info container-fluid text-center text-md-left">
+		<div class="row">
+			<div class="col-md-6 mt-md-0 mt-3">
+				<?php $blog_info = get_bloginfo( 'name' ); ?>
+				<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<?php endif; ?>
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'helfe' ); ?>">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'menu_class'     => 'footer-menu',
-							'depth'          => 1,
-						)
-					);
-					?>
-				</nav><!-- .footer-navigation -->
-			<?php endif; ?>
+				<?php endif; ?>
+			</div>
+      		<hr class="clearfix w-100 d-md-none pb-3">
+			<div class="col-md-3 mb-md-0 mb-3">
+				<?php if ( has_nav_menu( 'footer' ) ) : ?>
+					<nav class="footer-navigation list-unstyled" aria-label="<?php esc_attr_e( 'Footer Menu', 'helfe' ); ?>">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer',
+								'menu_class'     => 'footer-menu',
+								'depth'          => 1,
+							)
+						);
+						?>
+					</nav><!-- .footer-navigation -->
+				<?php endif; ?>
+			</div>
+		</div><!-- .row -->
 		</div><!-- .site-info -->
+		<div class="footer-copyright text-center py-3">© 2019 Copyright:
+			<a href="https://helfe.fr/"> Helfe </a>
+		</div>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
--- wp-content/themes/helfe/templates/header/header.php
+++ wp-content/themes/helfe/templates/header/header.php
@@ -8,7 +8,6 @@
  */
 ?>
 <div class="container-fluid">
-HEADER
     <?php if ( has_custom_logo() ) : ?>
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
