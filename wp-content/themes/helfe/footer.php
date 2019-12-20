b10d0d4245459beab4f5d1e1a95986d1b8b42614
vlav98 <valentinelav98@gmail.com>
2019-12-20 18:45:43 +0100

    :sparkles: Adding Social Menu to Footer

--- wp-content/themes/helfe/footer.php
+++ wp-content/themes/helfe/footer.php
@@ -10,20 +10,13 @@
  */
 
 ?>
-
 </div><!-- #content -->
 	<footer id="colophon" class="site-footer page-footer">
 		<div id="footer_helfe">
 			<div class="site-info container-fluid text-center text-md-left">
 			<div class="row">
 				<?php $blog_info = get_bloginfo( 'name' ); ?>
-				<?php if ( ! empty( $blog_info ) ) : ?>
-				<div class="col-md-12 mt-md-0 mt-3">
-					<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</div>
-				<?php endif; ?>
-				<hr class="clearfix w-100 d-md-none pb-3">
-				<div class="col-md-3 mb-md-0 mb-3">
+				<div class="col-md-4 mb-md-0 mb-4 align-self-start">
 					<?php if ( has_nav_menu( 'footer' ) ) : ?>
 						<nav class="footer-navigation list-unstyled" aria-label="<?php esc_attr_e( 'Footer Menu', 'helfe' ); ?>">
 							<?php
@@ -38,10 +31,26 @@
 						</nav><!-- .footer-navigation -->
 					<?php endif; ?>
 				</div>
+				<div class="col-md-4 mb-md-0 mb-4 align-self-end">
+					<?php if ( has_nav_menu( 'social' ) ) : ?>
+						<nav class="footer-navigation list-unstyled" aria-label="<?php esc_attr_e( 'Social Menu', 'helfe' ); ?>">
+							Suivez-nous sur nos réseaux sociaux
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'social',
+									'menu_class'     => 'social-menu',
+									'depth'          => 1,
+								)
+							);
+							?>
+						</nav><!-- .social-navigation -->
+					<?php endif; ?>
+				</div>
 			</div><!-- .row -->
 			</div><!-- .site-info -->
 			<div class="footer-copyright text-center py-3">© Copyright
-				<a href="https://helfe.fr/"> Helfe</a>
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				All Rights Reserved
 			</div>
 		</div>
