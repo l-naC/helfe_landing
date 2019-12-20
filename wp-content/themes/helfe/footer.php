0bf4255e2bbbc267709a64fe5165b65a418ce4f0
Léna Clavier <lenaclavier@macbook-pro-de-lena.home>
2019-12-20 13:15:52 +0100

    change footer css

--- wp-content/themes/helfe/footer.php
+++ wp-content/themes/helfe/footer.php
@@ -14,34 +14,37 @@
 </div><!-- #content -->
 	<footer id="colophon" class="site-footer page-footer">
 		<?php get_template_part( 'templates/footer/footer', 'widgets' ); ?>
-		<div class="site-info container-fluid text-center text-md-left">
-		<div class="row">
-			<div class="col-md-6 mt-md-0 mt-3">
+		<div id="footer_helfe">
+			<div class="site-info container-fluid text-center text-md-left">
+			<div class="row">
 				<?php $blog_info = get_bloginfo( 'name' ); ?>
 				<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<div class="col-md-12 mt-md-0 mt-3">
+					<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				</div>
 				<?php endif; ?>
+				<hr class="clearfix w-100 d-md-none pb-3">
+				<div class="col-md-3 mb-md-0 mb-3">
+					<?php if ( has_nav_menu( 'footer' ) ) : ?>
+						<nav class="footer-navigation list-unstyled" aria-label="<?php esc_attr_e( 'Footer Menu', 'helfe' ); ?>">
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'footer',
+									'menu_class'     => 'footer-menu',
+									'depth'          => 1,
+								)
+							);
+							?>
+						</nav><!-- .footer-navigation -->
+					<?php endif; ?>
+				</div>
+			</div><!-- .row -->
+			</div><!-- .site-info -->
+			<div class="footer-copyright text-center py-3">© Copyright
+				<a href="https://helfe.fr/"> Helfe</a>
+				All Rights Reserved
 			</div>
-      		<hr class="clearfix w-100 d-md-none pb-3">
-			<div class="col-md-3 mb-md-0 mb-3">
-				<?php if ( has_nav_menu( 'footer' ) ) : ?>
-					<nav class="footer-navigation list-unstyled" aria-label="<?php esc_attr_e( 'Footer Menu', 'helfe' ); ?>">
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'footer',
-								'menu_class'     => 'footer-menu',
-								'depth'          => 1,
-							)
-						);
-						?>
-					</nav><!-- .footer-navigation -->
-				<?php endif; ?>
-			</div>
-		</div><!-- .row -->
-		</div><!-- .site-info -->
-		<div class="footer-copyright text-center py-3">© 2019 Copyright:
-			<a href="https://helfe.fr/"> Helfe </a>
 		</div>
 	</footer><!-- #colophon -->
 
