357d8306a2f781aceea0bed74a9629b819c60fc5
Léna Clavier <lenaclavier@MacBook-Pro-de-Lena.local>
2020-01-31 09:47:31 +0100

    modify menu

--- wp-content/themes/helfe/index.php
+++ wp-content/themes/helfe/index.php
@@ -17,7 +17,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area container align-middle">
+	<div id="primary" class="content-area container align-middle" style="margin-top: 116px;">
 		<main id="main" class="site-main">
 
 		<?php
